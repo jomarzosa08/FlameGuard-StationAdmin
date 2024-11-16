34ac2d5072f75772fe170a941eb264d32f7e483a
Jomar Llarenas Zosa <zosajomar123@gmail.com>
2024-11-16 21:18:31 +0800

    changes in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,42 @@
 // src/App.jsx
 
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Dashboard from './pages/Dashboard';
+import React, { useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import Login from './pages/Login';
+import Dashboard from './pages/Dashboard';
 import UserProfiles from './pages/UserProfiles';
 import ViewMap from './pages/ViewMap';
 import RegisterForm from './components/RegisterForm';
-// other imports...
 
 const App = () => {
+    const location = useLocation();
+
+    useEffect(() => {
+        const routeTitleMap = {
+            "/": "Login - FlameGuard",
+            "/dashboard": "Dashboard - FlameGuard",
+            "/profiles": "User Profiles - FlameGuard",
+            "/viewmap": "View Map - FlameGuard",
+            "/register": "Register - FlameGuard",
+        };
+        document.title = routeTitleMap[location.pathname] || "FlameGuard";
+    }, [location]);
+
     return (
-        <Router>
-            <Routes>
-                <Route path="/" element={<Login />} />
-                <Route path="/dashboard" element={<Dashboard />} />
-                <Route path="/profiles" element={<UserProfiles />} />
-                <Route path="/viewmap" element={<ViewMap />} />
-                <Route path="/register" element={<RegisterForm />} />
-                {/* Add other routes here */}
-            </Routes>
-        </Router>
+        <Routes>
+            <Route path="/" element={<Login />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/profiles" element={<UserProfiles />} />
+            <Route path="/viewmap" element={<ViewMap />} />
+            <Route path="/register" element={<RegisterForm />} />
+        </Routes>
     );
 };
 
-export default App;
+const AppWrapper = () => (
+    <Router>
+        <App />
+    </Router>
+);
+
+export default AppWrapper;
